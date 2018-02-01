d4909a640796a03f1bfb2e960cbe2140f8e74561
hernanex3 <hernan.digiorgi@gmail.com>
2018-02-01 17:30:29 -0300

    erlang code & readme

---
+++ src/log_sup.erl
@@ -0,0 +1,15 @@
+-module(log_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    {ok, { {one_for_all, 0, 1}, []} }.
+
